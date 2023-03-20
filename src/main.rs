3cf7dc1935ac5ff70dbe489b51269883f5a70930
Loic Coyle <loic.coyle@hotmail.fr>
2023-03-20 23:43:04 +0100

    refactor: cleanup the exit and error msg handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::{error::Error, time::Duration};
+
 use clap::Parser;
-use evdev::Device;
-use tokio::{spawn, task::JoinHandle};
+use evdev::{Device, EventStream};
+use tokio::{spawn, task::JoinHandle, time::sleep};
 use tokio_stream::{StreamExt, StreamMap};
 
 mod cli;
@@ -10,66 +12,13 @@ mod terminal;
 mod uinput;
 mod utils;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = cli::Arguments::parse();
-    env_logger::Builder::new()
-        .filter_level(args.verbose.log_level_filter())
-        .init();
-    log::debug!("args: {:?}", args);
-
-    let pre_cmd = shlex::split(&args.pre_cmd).ok_or("Failed to parse command")?;
-
-    let uid = permissions::get_caller_uid()?;
-    log::debug!("Caller UID: {}", uid);
-
-    // setup uinput virtual device
-    let virt_device = uinput::create_uinput_device().unwrap_or_else(|e| {
-        log::error!("Failed to create uinput device: {}", e);
-        std::process::exit(1);
-    });
-
-    // fetch keyboards
-    let keyboard_devices: Vec<Device> = evdev::enumerate()
-        .map(|(_, device)| device)
-        .filter(utils::check_device_is_keyboard)
-        .collect();
-
-    if keyboard_devices.is_empty() {
-        log::error!("No keyboard found");
-        std::process::exit(1);
-    }
-
-    log::info!("Found {} keyboards", keyboard_devices.len());
-    if log::log_enabled!(log::Level::Debug) {
-        for device in keyboard_devices.iter() {
-            log::debug!("Device: {:?}", device.name());
-        }
-    }
-
-    let mut stream_map = StreamMap::new();
-    // Grab the keyboards and feed their streams into `stream_map`.
-    for (i, mut device) in keyboard_devices.into_iter().enumerate() {
-        let _ = device.grab();
-        stream_map.insert(i, device.into_event_stream()?);
-    }
-    // Setup keyboard
-    let config = terminal::TerminalConfig {
-        pre_cmd,
-        output_method: if args.type_output {
-            terminal::OutputMethod::Type
-        } else {
-            terminal::OutputMethod::Paste
-        },
-        key_delay: args.key_delay,
-    };
-    let mut keyboard = keyboard::Keyboard::new();
-    let mut terminal = terminal::Terminal::new(virt_device, config).unwrap_or_else(|e| {
-        log::error!("Failed to create terminal: {}", e);
-        std::process::exit(1);
-    });
+async fn handle_events(
+    uid: u32,
+    mut keyboard: keyboard::Keyboard,
+    mut terminal: terminal::Terminal,
+    mut stream_map: StreamMap<usize, EventStream>,
+) -> Result<(), Box<dyn Error>> {
     let mut cmd_task: Option<JoinHandle<()>> = None;
-
     log::info!("Listening for keyboard events...");
     log::info!("Ctrl-C/ESC to exit");
     // Event loop
@@ -88,28 +37,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     // Re-emit all key releases
                     terminal.emit(&[event]).unwrap_or_else(|e| {
                         log::error!("Failed to emit key: {}", e);
-                        std::process::exit(1);
                     });
                 } else if cmd_task.is_none() {
                     // don't update the terminal state if cmd is running
                     // Re-emit key presses based on the terminal state and capabilities
-                    match terminal
-                        .handle_key(key, keyboard.is_shift())
-                        .unwrap_or_else(|e| {
-                            log::error!("Failed to handle key: {}", e);
-                            std::process::exit(1);
-                        }) {
+                    match terminal.handle_key(key, keyboard.is_shift())? {
                         terminal::EventFlag::Emit => {
                             log::debug!("Passing through {:?}", event);
                             // here we emit the event as a single key press regardless of if it was a held down
                             // key or not. This is because we are not handling key repeats. And allows the
                             // grabbed keyboard to decide the rates of the virtual device.
-                            terminal
-                                .send_key(key, keyboard.is_shift())
-                                .unwrap_or_else(|e| {
-                                    log::error!("Failed to send key: {}", e);
-                                    std::process::exit(1);
-                                });
+                            terminal.send_key(key, keyboard.is_shift())?
                         }
                         terminal::EventFlag::Block => {}
                     }
@@ -117,40 +55,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                 if keyboard.is_ctrl_c() || keyboard.is_escape() {
                     log::info!("Ctrl-C/ESC detected, exiting...");
-                    terminal.clear().unwrap_or_else(|e| {
-                        log::error!("Failed to clear terminal: {}", e);
-                        std::process::exit(1);
-                    });
-                    utils::release_keyboards();
                     if let Some(task) = cmd_task {
                         log::info!("Killing running command");
+                        // TODO: this does not actually stop the command from running
                         task.abort();
                     }
-                    std::process::exit(0);
+                    terminal.clear()?;
+                    break;
                 } else if keyboard.is_enter() && cmd_task.is_none() {
-                    log::info!("Enter detected, running command and writing output...");
-                    permissions::drop_privileges(uid).unwrap_or_else(|e| {
-                        log::error!("Failed to drop privileges: {}", e);
-                        terminal.clear().unwrap_or_else(|e| {
-                            log::error!("Failed to clear terminal: {}", e);
-                        });
-                        std::process::exit(1);
-                    });
+                    permissions::drop_privileges(uid)?;
                     log::debug!("Dropped privileges");
                     let runner = terminal.clone();
                     cmd_task = Some(spawn(async move {
                         let out = runner.run(uid).await;
-                        // print the memmory address
+                        // TODO: improve this, this is a hack to allow for the task to be aborted
+                        sleep(Duration::from_millis(10)).await;
                         match out {
                             Ok(out) => {
                                 log::info!("Command ran successfully");
                                 runner.write(out).unwrap_or_else(|e| {
                                     log::error!("Failed to write output: {}", e);
                                 });
+                                // TODO: It would be good to handle these exits in the main function
                                 utils::release_keyboards();
                                 std::process::exit(0);
                             }
-
                             Err(e) => {
                                 log::error!("Command failed: {}", e);
                                 runner
@@ -158,6 +87,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                                     .unwrap_or_else(|e| {
                                         log::error!("Failed to write output: {}", e);
                                     });
+                                // TODO: It would be good to handle these exits in the main function
                                 utils::release_keyboards();
                                 std::process::exit(1);
                             }
@@ -165,14 +95,72 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     }));
                 }
             }
-            evdev::InputEventKind::Synchronization(_) => {
-                terminal.emit(&[event]).unwrap_or_else(|e| {
-                    log::error!("Failed to emit event: {}", e);
-                    std::process::exit(1);
-                })
-            }
+            evdev::InputEventKind::Synchronization(_) => terminal.emit(&[event])?,
             _ => {}
         }
     }
     Ok(())
+}
+
+async fn _main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = cli::Arguments::parse();
+    env_logger::Builder::new()
+        .filter_level(args.verbose.log_level_filter())
+        .init();
+    log::debug!("args: {:?}", args);
+
+    let pre_cmd = shlex::split(&args.pre_cmd).ok_or("Failed to parse command")?;
+
+    let uid = permissions::get_caller_uid()?;
+    log::debug!("Caller UID: {}", uid);
+
+    // setup uinput virtual device
+    let virt_device = uinput::create_uinput_device()?;
+
+    // fetch keyboards
+    let keyboard_devices: Vec<Device> = evdev::enumerate()
+        .map(|(_, device)| device)
+        .filter(utils::check_device_is_keyboard)
+        .collect();
+
+    if keyboard_devices.is_empty() {
+        return Err("No keyboard found".into());
+    }
+
+    log::info!("Found {} keyboards", keyboard_devices.len());
+    if log::log_enabled!(log::Level::Debug) {
+        for device in keyboard_devices.iter() {
+            log::debug!("Device: {:?}", device.name());
+        }
+    }
+
+    let mut stream_map = StreamMap::new();
+    // Grab the keyboards and feed their streams into `stream_map`.
+    for (i, mut device) in keyboard_devices.into_iter().enumerate() {
+        device.grab()?;
+        stream_map.insert(i, device.into_event_stream()?);
+    }
+    let config = terminal::TerminalConfig {
+        pre_cmd,
+        output_method: if args.type_output {
+            terminal::OutputMethod::Type
+        } else {
+            terminal::OutputMethod::Paste
+        },
+        key_delay: args.key_delay,
+    };
+    let keyboard = keyboard::Keyboard::new();
+    let terminal = terminal::Terminal::new(virt_device, config)?;
+    handle_events(uid, keyboard, terminal, stream_map).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let status = _main().await;
+    utils::release_keyboards();
+    status.unwrap_or_else(|e| {
+        log::error!("Failed to run: {}", e);
+        std::process::exit(1);
+    });
 }
